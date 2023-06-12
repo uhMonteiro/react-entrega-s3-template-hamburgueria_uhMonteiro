2a9c34ee61421e51c7b29d7f36b94bab31be9c63
Augusto <augustomonteirop@hotmail.com>
2023-06-11 23:43:18 -0300

    itens da api

--- src/components/SnacksCard/index.jsx
+++ src/components/SnacksCard/index.jsx
@@ -1,30 +1,16 @@
-import axios from "axios"
-import { useEffect } from "react"
-import { api } from "../../services/api"
-import { useState } from "react"
 
 
-export function SnacksCards(){
-    const [products , setProducts] = useState([])
+export function SnacksCards({ product }){
     
-    useEffect(() => {
-        async function getProducts(){
-            const response = await api.get('/products')
-            console.log(response)
-        }
-        getProducts()
-        
-    } , [])
-
     return(
         <li>
             <div>
-                <img src="" alt="" />
+                <img src={product.img} alt={product.name} />
             </div>
             <div>
-                <h2></h2>
-                <p></p>
-                <p></p>
+                <h2>{product.name}</h2>
+                <p>{product.category}</p>
+                <p>{product.price}</p>
                 <button>Adicionar</button>
             </div>
         </li>
--- src/components/SnacksSection/index.jsx
+++ src/components/SnacksSection/index.jsx
@@ -1,11 +1,28 @@
 import { SnacksCards } from "../SnacksCard";
+import { useEffect } from "react"
+import { api } from "../../services/api"
+import { useState } from "react"
+
 
 
 export function SnacksSection(){
+    const [products , setProducts] = useState([])
+    
+    useEffect(() => {
+        async function getProducts(){
+            const response = await api.get('/products')
+
+            setProducts(response.data)
+            console.log(response)
+        }
+        getProducts()
+        
+    } , [])
+
     return(
         <section>
             <ul>
-                <SnacksCards/>
+                {products.map((product) => <SnacksCards key={product.id} product={product}/>)}
             </ul>
         </section>
     )
