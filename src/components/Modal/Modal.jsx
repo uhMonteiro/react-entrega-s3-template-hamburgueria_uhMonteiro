8a5c0de526d98e6759eb28fcd6a88034f1fdc347
Augusto <augustomonteirop@hotmail.com>
2023-06-13 14:47:39 -0300

    bug no audio

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,30 @@
+import { BackgroundModalStyle, SectionModalStyle } from "./style";
 
 
 export function Modal(){
     return(
-        <div>
-            <p>modal</p>
-        </div>
+        <BackgroundModalStyle>
+         <SectionModalStyle>
+            <header>
+                <h2>Carrinho de compras</h2>
+            </header>
+            <div>
+                <ul>
+                    <img src="" alt="" />
+                    <h2></h2>
+                    <button></button>
+
+                </ul>
+                <div>
+                    <span>
+                        <h3>Total</h3>
+                        <p>R$</p>
+                    </span>
+                    <button>Remover todos</button>
+                </div>
+            </div>
+
+         </SectionModalStyle>
+        </BackgroundModalStyle>
     )
 }
---
+++ src/components/Modal/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const BackgroundModalStyle= styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgb(0,0,0,0.7);
+`
+
+
+export const SectionModalStyle = styled.section`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    width: 500px;
+    height: 371px;
+`
